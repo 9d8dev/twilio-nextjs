7ccb9e330e1c8a01409c4b922972c96d8c28aca6
Cameron Youngblood <cameron.youngblood@gmail.com>
2023-06-06 15:09:29 -0600

    add conditional rendering

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import styles from "./styles.module.css";
 export default function Home() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [twilioCode, setTwilioCode] = useState("");
+  const [visible, setVisible] = useState(false);
 
   const handlePhoneNumberChange = (e) => {
     const formattedPhoneNumber = formatPhoneNumber(e.target.value);
@@ -16,43 +17,49 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-12 lg:p-24">
       <div className={styles.neu}>
         <div className="w-max">
-          <form>
-            <div className="form-control w-full max-w-xs">
-              <label className="label" htmlFor="phoneNumber">
-                <span className="label-text uppercase">Phone Number</span>
-              </label>
-              <input
-                name="phoneNumber"
-                type="text"
-                placeholder="(222)-222-2222"
-                className="input input-bordered w-full max-w-xs"
-                value={phoneNumber}
-                onChange={handlePhoneNumberChange}
-                required
-              />
-            </div>
-            <button className="btn btn-wide my-4">Submit</button>
-          </form>
+          {!visible ? (
+            <form>
+              <div className="form-control w-full max-w-xs">
+                <label className="label" htmlFor="phoneNumber">
+                  <span className="label-text uppercase">Phone Number</span>
+                </label>
+                <input
+                  name="phoneNumber"
+                  type="text"
+                  placeholder="(222)-222-2222"
+                  className="input input-bordered w-full max-w-xs"
+                  value={phoneNumber}
+                  onChange={handlePhoneNumberChange}
+                  required
+                />
+              </div>
+              <button className="btn btn-wide my-4">Submit</button>
+            </form>
+          ) : (
+            <>
+              <div className="w-full lg:w-1/2">
+                <div className="divider"></div>
+              </div>
 
-          <div className="w-full lg:w-1/2">
-            <div className="divider"></div>
-          </div>
-
-          <form>
-            <div className="form-control w-full max-w-xs">
-              <label className="label" htmlFor="twilioCode">
-                <span className="label-text uppercase">Verification Code</span>
-              </label>
-              <input
-                name="twilioCode"
-                type="text"
-                placeholder="222-222"
-                className="input input-bordered w-full max-w-xs"
-                required
-              />
-            </div>
-            <button className="btn btn-wide my-4">Submit</button>
-          </form>
+              <form>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label" htmlFor="twilioCode">
+                    <span className="label-text uppercase">
+                      Verification Code
+                    </span>
+                  </label>
+                  <input
+                    name="twilioCode"
+                    type="text"
+                    placeholder="222-222"
+                    className="input input-bordered w-full max-w-xs"
+                    required
+                  />
+                </div>
+                <button className="btn btn-wide my-4">Submit</button>
+              </form>
+            </>
+          )}
         </div>
       </div>
     </main>
