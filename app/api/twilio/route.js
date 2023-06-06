407bc1a4b3e2935bc4b8d11cfae294e11ab05933
Cameron Youngblood <cameron.youngblood@gmail.com>
2023-06-06 15:10:54 -0600

    Create Twilio API Route

---
+++ app/api/twilio/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import twilio from "twilio";
+const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const phoneNumber = searchParams.get("phoneNumber");
+  try {
+    const verification = await client.verify.v2
+      .services(process.env.VERIFICATION_SID)
+      .verifications.create({ to: phoneNumber, channel: "sms" });
+
+    return NextResponse.json(verification.status);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error("Something went wrong during verification.");
+  }
+}
+
+export async function POST(request) {
+  const { code, phoneNumber } = await request.json();
+  try {
+    const verificationResult = await client.verify.v2
+      .services(process.env.VERIFICATION_SID)
+      .verificationChecks.create({ to: phoneNumber, code: code });
+
+    return NextResponse.json(verificationResult.status);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error("Something went wrong during verification.");
+  }
+}
